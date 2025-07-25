3ca1e0b345a1403dc988949371ac7ab79cb26865
Samyak Nidhi <168972668+Samyaknidhi@users.noreply.github.com>
2025-07-25 10:04:22 +0530

    Delete adc_join.h

--- adc_join.h
+++ adc_join.h
@@ -1,17 +1,1 @@
-#ifndef ADC_HERE_F_H
-#define ADC_HERE_F_H
 
-#include <ap_int.h>
-#include <ap_fixed.h>
-
-#define N 512
-#define W 16  // Output bit width
-
-void dual_adc_system(
-    ap_fixed<16,4> I_analog_in[N],
-    ap_fixed<16,4> Q_analog_in[N],
-    ap_int<W> I_digital_out[N],
-    ap_int<W> Q_digital_out[N]
-);
-
-#endif // ADC_HERE_F_H
